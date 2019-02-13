760f4d8a43778af1c3231a4ceab883513eed75e1
Dallin Smith <dallinbsmith@gmail.com>
2019-02-13 14:25:09 -0700

    login

--- pluralsight/vue-spa/src/app.service.js
+++ pluralsight/vue-spa/src/app.service.js
@@ -10,7 +10,17 @@ const appService = {
           resolve(response.data)
       })
     })
-  }
+  },
+  // login(credentials){
+  //   return new Promise((resolve,reject) => {
+  //     axios.post('services/auth.php', credentials)
+  //     .then(response => {
+  //       resolve(response.data)
+  //     }).catch(response => {
+  //       reject(response.status)
+  //     })
+  //   })
+  // }
 }
 
 export default appService
