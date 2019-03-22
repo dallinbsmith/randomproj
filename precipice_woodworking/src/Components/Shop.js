95d7eb42306467bc45646cc914f5ebbbad7e0611
Dallin Smith <dallinbsmith@gmail.com>
2019-03-22 11:46:08 -0600

    I don't even know everything I  did here

--- precipice_woodworking/src/Components/Shop.js
+++ precipice_woodworking/src/Components/Shop.js
@@ -11,7 +11,7 @@ class Shop extends Component {
     render() {
         return (
             <div>
-                <h1>blah blah</h1>
+                <h1>Testing test</h1>
             </div>
         );
     }
